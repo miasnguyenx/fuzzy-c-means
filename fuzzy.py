a683c2848123ce9e3bbf627c365f1235c8e1099a
miasnguyenx <miasnguyenx@gmail.com>
2021-10-26 08:47:53 +0700

    Add distances matrix

--- fuzzy.py
+++ fuzzy.py
@@ -4,19 +4,20 @@ import random
 import operator
 import math
 import matplotlib.pyplot as plt
-from scipy.stats import multivariate_normal     # for generating pdf
+from scipy.spatial import distance
+from scipy.stats import multivariate_normal  # for generating pdf
 
 
 df_full = pd.read_csv("Iris.csv")  # iris data
 df_full.head()
 
-df_full = df_full.drop(['Id'], axis=1)
+df_full = df_full.drop(["Id"], axis=1)
 df_full.shape
 
 columns = list(df_full.columns)  # column now is an array variable
 # print(columns)
 # features is column without species attribute
-features = columns[:len(columns)-1]
+features = columns[: len(columns) - 1]
 # print(features)
 class_labels = list(df_full[columns[-1]])
 # print(class_labels)
@@ -45,20 +46,20 @@ e = 0.01
 # print(temp_list)
 
 plt.figure(figsize=(10, 10))  # scatter plot of sepal length vs sepal width
-plt.scatter(list(df.iloc[:, 0]), list(df.iloc[:, 1]), marker='o')
-plt.axis('equal')
-plt.xlabel('Sepal Length', fontsize=16)
-plt.ylabel('Sepal Width', fontsize=16)
-plt.title('Sepal Plot', fontsize=22)
+plt.scatter(list(df.iloc[:, 0]), list(df.iloc[:, 1]), marker="o")
+plt.axis("equal")
+plt.xlabel("Sepal Length", fontsize=16)
+plt.ylabel("Sepal Width", fontsize=16)
+plt.title("Sepal Plot", fontsize=22)
 plt.grid()
 plt.show()
 
 plt.figure(figsize=(10, 10))  # scatter plot of petal length vs sepal width
-plt.scatter(list(df.iloc[:, 2]), list(df.iloc[:, 3]), marker='o')
-plt.axis('equal')
-plt.xlabel('Petal Length', fontsize=16)
-plt.ylabel('Petal Width', fontsize=16)
-plt.title('Petal Plot', fontsize=22)
+plt.scatter(list(df.iloc[:, 2]), list(df.iloc[:, 3]), marker="o")
+plt.axis("equal")
+plt.xlabel("Petal Length", fontsize=16)
+plt.ylabel("Petal Width", fontsize=16)
+plt.title("Petal Plot", fontsize=22)
 plt.grid()
 plt.show()
 
@@ -72,14 +73,23 @@ def accuracy(cluster_labels, class_labels):
     virg = max(set(labels[100:]), key=labels[100:].count)
 
     for i in range(len(df)):
-        if cluster_labels[i] == seto and class_labels[i] == 'Iris-setosa':
+        if cluster_labels[i] == seto and class_labels[i] == "Iris-setosa":
             correct_pred = correct_pred + 1
-        if cluster_labels[i] == vers and class_labels[i] == 'Iris-versicolor' and vers != seto:
+        if (
+            cluster_labels[i] == vers
+            and class_labels[i] == "Iris-versicolor"
+            and vers != seto
+        ):
             correct_pred = correct_pred + 1
-        if cluster_labels[i] == virg and class_labels[i] == 'Iris-virginica' and virg != seto and virg != vers:
+        if (
+            cluster_labels[i] == virg
+            and class_labels[i] == "Iris-virginica"
+            and virg != seto
+            and virg != vers
+        ):
             correct_pred = correct_pred + 1
 
-    accuracy = correct_pred/len(df)*100
+    accuracy = correct_pred / len(df) * 100
     return accuracy
 
 
@@ -87,16 +97,15 @@ def initializeMembershipMatrix():  # initializing the membership matrix
     membership_mat = []
     # initialize the membership maxtrix for each objects
     for i in range(n):
-        random_num_list = [random.random()
-                           for i in range(k)]  # random a k-values array
+        random_num_list = [random.random() for i in range(k)]  # random a k-values array
 
         summation = sum(random_num_list)  # sum of all values in array
-        temp_list = [x/summation for x in random_num_list]
+        temp_list = [x / summation for x in random_num_list]
 
         # attach the object to a cluster (0,1,2) - respectively to three iris categories
         flag = temp_list.index(max(temp_list))
         for j in range(0, len(temp_list)):
-            if(j == flag):
+            if j == flag:
                 temp_list[j] = 1
             else:
                 temp_list[j] = 0
@@ -161,7 +170,7 @@ def calculateClusterCenter(membership_mat):  # calculating the cluster center
             temp_num.append(prod)
         # sum of list(zip(*temp_num)) is numerator of v[i]
         numerator = map(sum, list(zip(*temp_num)))
-        center = [z/denominator for z in numerator]
+        center = [z / denominator for z in numerator]
         cluster_centers.append(center)
     return cluster_centers
 
@@ -180,16 +189,37 @@ cluster_centers = calculateClusterCenter(membership_mat)
 
 
 def updateMembershipValue(membership_mat, cluster_centers):
-    p = float(2/(m-1))
+
+    p = float(2 / (m - 1))
     for i in range(n):
         x = list(df.iloc[i])
-        distances = [np.linalg.norm(
-            np.array(list(map(operator.sub, x, cluster_centers[j])))) for j in range(k)]
+        distances = [
+            np.linalg.norm(np.array(list(map(operator.sub, x, cluster_centers[j]))))
+            for j in range(k)
+        ]
         for j in range(k):
-            den = sum([math.pow(float(distances[j]/distances[c]), p)
-                       for c in range(k)])
-            membership_mat[i][j] = float(1/den)
+            den = sum(
+                [math.pow(float(distances[j] / distances[c]), p) for c in range(k)]
+            )
+            membership_mat[i][j] = float(1 / den)
     return membership_mat
+
+
+def initDistancesMatrix():
+    rows, cols = (150, 150)
+    dst = [[0 for i in range(cols)] for j in range(rows)]
+    for i in range(n):
+        x = list(df.iloc[i])
+        for j in range(n):
+            y = list(df.iloc[j])
+            dst[i][j] = distance.euclidean(x, y)
+
+    # print(np.array(dst[1]))
+    # print(np.array(dst[0]))
+
+
+initDistancesMatrix()
+
 
 # cluster_labels = list()
 # for i in range(n):
@@ -222,14 +252,17 @@ def fuzzyCMeansClustering():  # Third iteration Random vectors from data
 
         acc.append(cluster_labels)
 
-        if(curr == 0):
+        if curr == 0:
             print("Cluster Centers:")
             print(np.array(cluster_centers))
         curr += 1
     print("---------------------------")
     print("Partition matrix:")
-    print(np.array(membership_mat))
-    print(cluster_labels)
+    # for i in range (0,149):
+    # print(np.max(membership_mat[i]), np.array(membership_mat)[i], cluster_labels[i])
+
+    print(np.array(cluster_labels))
+    # print(np.array(cluster_labels).shape)
     # return cluster_labels, cluster_centers
     return cluster_labels, cluster_centers, acc
 
@@ -237,8 +270,9 @@ def fuzzyCMeansClustering():  # Third iteration Random vectors from data
 labels, centers, acc = fuzzyCMeansClustering()
 a = accuracy(labels, class_labels)
 
-print(labels[1])
-print(class_labels[0])
+
+# print(labels[1])
+# print(class_labels[0])
 # P.S. The accuracy calculation is for iris data only
 
 # acc_lis = []
@@ -256,8 +290,9 @@ print("Accuracy = ", a)
 # accuracy = accuracy(labels, class_labels)
 # print("---------------------------")
 # print("Accuracy: " ,accuracy)
-print("Cluster center vectors:") #final cluster centers
+print("Cluster center vectors:")  # final cluster centers
 print(np.array(centers))
+print(list(df.iloc[1]))
 # sepal_df = df_full.iloc[:,0:2]
 # sepal_df = np.array(sepal_df)
 # #m1 = [0,0]
@@ -276,26 +311,26 @@ print(np.array(centers))
 # cov2 = np.cov(np.transpose(sepal_df))
 # cov3 = np.cov(np.transpose(sepal_df))
 
-# x1 = np.linspace(4,8,150)  
+# x1 = np.linspace(4,8,150)
 # x2 = np.linspace(1.5,4.5,150)
-# #x1 = np.linspace(-1,8,150)  
+# #x1 = np.linspace(-1,8,150)
 # #x2 = np.linspace(-1,4.5,150)
-# X, Y = np.meshgrid(x1,x2) 
+# X, Y = np.meshgrid(x1,x2)
 
-# Z1 = multivariate_normal(m1, cov1)  
+# Z1 = multivariate_normal(m1, cov1)
 # Z2 = multivariate_normal(m2, cov2)
 # Z3 = multivariate_normal(m3, cov3)
 
 # pos = np.empty(X.shape + (2,)) ## a new array of given shape and type, without initializing entries
 
-# pos[:, :, 0] = X; pos[:, :, 1] = Y   
+# pos[:, :, 0] = X; pos[:, :, 1] = Y
 
 # plt.figure(figsize=(10,10)) # creating the figure and assigning the size
-# plt.scatter(sepal_df[:,0], sepal_df[:,1], marker='o')     
-# plt.contour(X, Y, Z1.pdf(pos), colors="r" ,alpha = 0.5) 
-# plt.contour(X, Y, Z2.pdf(pos), colors="b" ,alpha = 0.5) 
-# plt.contour(X, Y, Z3.pdf(pos), colors="g" ,alpha = 0.5) 
-# plt.axis('equal') # making both the axis equal                                                                 
+# plt.scatter(sepal_df[:,0], sepal_df[:,1], marker='o')
+# plt.contour(X, Y, Z1.pdf(pos), colors="r" ,alpha = 0.5)
+# plt.contour(X, Y, Z2.pdf(pos), colors="b" ,alpha = 0.5)
+# plt.contour(X, Y, Z3.pdf(pos), colors="g" ,alpha = 0.5)
+# plt.axis('equal') # making both the axis equal
 # plt.xlabel('Sepal Length', fontsize=16) # X-Axis
 # plt.ylabel('Sepal Width', fontsize=16)  # Y-Axis
 # plt.title('Initial Random Clusters(Sepal)', fontsize=22) # Title of the plot
---
+++ test.py
@@ -0,0 +1,7 @@
+n = 150
+dst =
+for i in range(n):
+        x = i 
+        for j in range(n):
+            y = j
+            dst[i][j] = i + j
